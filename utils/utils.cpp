6d4341218644f8e4dab401641d1381f390471e93
Ciaran KG <55831661+Eldred777@users.noreply.github.com>
2024-05-29 22:13:04 +1000

    reformat

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -2,21 +2,27 @@
 #include <iostream>
 #include <string>
 
-void handle_file_stream(std::ifstream& is, std::string rel_filepath) {
+void handle_file_stream(std::ifstream &is, std::string rel_filepath) {
   is.open(rel_filepath);
-
+  if (is.is_open()) {
+    return;
+  }
+  is.close();
+  // 6 for "input"
+  int input_name_idx = rel_filepath.find('/', 6) + 1;
+  is.open(rel_filepath.substr(input_name_idx));
   if (!is.is_open()) {
-    is.close();
-    int input_name_idx = rel_filepath.find('/', 6) + 1;
-    is.open(rel_filepath.substr(input_name_idx));
-    if (!is.is_open()) {
-      std::cerr << "Input file stream failed to open.";
-      exit(1);
-    }
+    std::cerr << "Input file stream failed to open.";
+    exit(1);
   }
 }
 
-void reset_file_stream(std::ifstream& is) {
+void reset_file_stream(std::ifstream &is) {
   is.clear();
   is.seekg(0, std::ios::beg);
+}
+
+void report_results(int p1, int p2) {
+  std::cout << "Part 1: " << p1 << '\n';
+  std::cout << "Part 2: " << p2 << std::endl;
 }
