094e714d66fe3deb8ca7d76b006f5b127742b503
Ciaran KG <55831661+Eldred777@users.noreply.github.com>
2025-01-16 16:34:35 +1000

    clj 15d2

--- langs/clj/src/clj_aoc/utils.clj
+++ langs/clj/src/clj_aoc/utils.clj
@@ -1,8 +1,14 @@
-(ns clj-aoc.utils)
+(ns clj-aoc.utils
+  (:require [clojure.string :as str]))
 
 (defn get-input
   "Reads the input file and returns a list of strings. Requires the year as yy and day as dd formats e.g. (get-input 15 1)"
   [year day]
   (slurp (str "../../inputs/" year "day" day)))
+
+(defn get-input-lines
+  "Splits the input at newline chars"
+  [input]
+  (str/split input #"\r?\n"))
 
 ;; TODO function to grab the input from the website
---
+++ langs/clj/src/clj_aoc/y15/d2.clj
@@ -0,0 +1,52 @@
+(ns clj-aoc.y15.d2
+  (:require [clj-aoc.utils :as utils]
+            [clojure.string :as str]))
+
+(defn process-dimension
+  "Processes `dimension-str` to a list of dimensions."
+  [dimension-str]
+  (as-> dimension-str s
+    (str/split s #"x")
+    (map #(Integer/parseInt %) s)))
+
+(defn process-input [input]
+  (as-> input i
+    (utils/get-input-lines i)
+    (map process-dimension i)))
+
+(defn face-areas [dimension-list]
+  (let [l (nth dimension-list 0)
+        w (nth dimension-list 1)
+        h (nth dimension-list 2)]
+    (list (* l w)
+          (* w h)
+          (* l h))))
+
+(defn part1 [dims]
+  (let [areas (map face-areas dims)]
+    (+ (reduce + (map #(apply min %) areas))
+       (* 2 (reduce + (map #(reduce + %) areas))))))
+
+(defn vol [dimensions]
+  (apply * dimensions))
+
+(defn least-perimeter [dimensions]
+  (let [l (nth dimensions 0)
+        w (nth dimensions 1)
+        h (nth dimensions 2)]
+    (apply min (list (* 2 (+ l w))
+                     (* 2 (+ w h))
+                     (* 2 (+ l h))))))
+
+(defn part2 [dims]
+  (reduce + (map  +
+                  (map vol dims)
+                  (map least-perimeter dims))))
+
+(defn solve []
+  (let [input (utils/get-input 15 2)
+        dims (process-input input)]
+    (println (str "Part 1: " (part1 dims)))
+    (println (str "Part 2: " (part2 dims)))))
+
+(solve)
